12e1f08b151522b742242f91743d1dc9614bbb85
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-05-12 14:05:59 -0400

    initial commit

---
+++ ActiveDirectory/Get-ComputersInDefaultContainer.ps1
@@ -0,0 +1,25 @@
+﻿#This script finds computers that have been left in the default "Computers" OU
+if ((Get-WmiObject Win32_ComputerSystem).domainrole -eq 5) {
+    #computer is a PDC
+    try {
+        Import-Module ActiveDirectory
+
+        $DefaultComputersDN = "CN=Computers,$(Get-ADDomain | Select-Object -ExpandProperty DistinguishedName)"
+
+        $ComputersInDefaultContainer = Get-ADComputer -SearchBase $DefaultComputersDN -Filter *
+
+        if ($ComputersInDefaultContainer.count -eq 0) {
+            #exit
+        }
+        else {
+            return "The Following computers were found in the Computers container: $($ComputersInDefaultContainer.Name -join ',')"
+        }
+    }
+    catch {
+        exit
+    }
+}
+else {
+    #not a PDC
+    exit
+}
