60006097816b928fab834889f04ad065389e9520
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-05-18 15:01:51 -0400

    moved to private

---
+++ RDS/FSLogix/Remove-LocalUPDProfiles.ps1
@@ -0,0 +1,89 @@
+<#
+
+This script deletes local profile data on an RDS server that has UPDs or FSL Profiles. When UPDs are enabled user files will sometimes be left behind. This can cause some unexpected resuults with applications.
+
+The script has a failsafe that does not run if UPDs or FSL Profiles become disabled at any point.
+
+Andy Morales
+#>
+function Test-RegistryValue {
+    <#
+    Checks if a reg key/value exists
+
+    #Modified version of the function below
+    #https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
+
+    Andy Morales
+    #>
+
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true,
+            Position = 1,
+            HelpMessage = 'HKEY_LOCAL_MACHINE\SYSTEM')]
+        [ValidatePattern('Registry::.*|HKEY_')]
+        [ValidateNotNullOrEmpty()]
+        [String]$Path,
+
+        [parameter(Mandatory = $true,
+            Position = 2)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
+
+        [parameter(Position = 3)]
+        [ValidateNotNullOrEmpty()]$ValueData
+    )
+
+    #Add Regdrive if it is not present
+    if ($Path -notmatch 'Registry::.*') {
+        $Path = 'Registry::' + $Path
+    }
+
+    try {
+        #Reg key with value
+        if ($ValueData) {
+            if ((Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop) -eq $ValueData) {
+                return $true
+            }
+            else {
+                return $false
+            }
+        }
+        #Key key without value
+        else {
+            $RegKeyCheck = Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop
+            if ($null -eq $RegKeyCheck) {
+                #if the Key Check returns null then it probably means that the key does not exist.
+                return $false
+            }
+            else {
+                return $true
+            }
+        }
+    }
+    catch {
+        return $false
+    }
+}
+
+#Array that will store the command and all parameters
+$CommandToExecute = @()
+
+#The basic command
+$CommandToExecute += 'C:\BIN\DelProf2\DelProf2.exe /u'
+
+#UPDs
+if (Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Terminal Server\ClusterSettings' -Name UvhdEnabled -Value 1) {
+    #UvhdCleanupBin is also excluded since it is unknown what deleting it will do
+    $CommandToExecute += '/ed:UvhdCleanupBin'
+}
+
+#FSL Profiles
+if (Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -ValueData 1) {
+    #Exclude local_ since FSL Profiles create some local items
+    $CommandToExecute += '/ed:local_*'
+}
+
+Invoke-Expression -Command ($CommandToExecute -join ' ')
+#Run the command again, but remove the local_ exclusion. Replace it with delete profiles older than 3 days. This deletes older local_ files that should not be in use.
+Invoke-Expression -Command ($CommandToExecute -join ' ').Replace('/ed:local_*','/d:3')
---
+++ RDS/FSLogix/Remove-LocalUPDProfilesScheduledTask.ps1
@@ -0,0 +1,96 @@
+<#
+
+.SYNOPSIS
+This script deletes local profile data on an RDS server that has UPDs. When UPDs are enabled user files will sometimes be left behind. This can cause some unexpected resuults with applications.
+
+The script has a failsafe that does not create the task if UPDs become disabled at any point.
+
+The script needs Remove-LocalUPDProfiles.ps1 to work correctly.
+
+.PARAMETER ScheduledTaskName
+This is the name that will be given to the ScheduledTask
+
+Andy Morales
+#>
+
+
+$ScheduledTaskName = 'Delete UPD Local Files'
+
+function Test-RegistryValue {
+    <#
+    Checks if a reg key/value exists
+
+    #Modified version of the function below
+    #https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
+
+    Andy Morales
+    #>
+
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true,
+            Position = 1,
+            HelpMessage = 'HKEY_LOCAL_MACHINE\SYSTEM')]
+        [ValidatePattern('Registry::.*|HKEY_')]
+        [ValidateNotNullOrEmpty()]
+        [String]$Path,
+
+        [parameter(Mandatory = $true,
+            Position = 2)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
+
+        [parameter(Position = 3)]
+        [ValidateNotNullOrEmpty()]$ValueData
+    )
+
+    #Add Regdrive if it is not present
+    if ($Path -notmatch 'Registry::.*') {
+        $Path = 'Registry::' + $Path
+    }
+
+    try {
+        #Reg key with value
+        if ($ValueData) {
+            if ((Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop) -eq $ValueData) {
+                return $true
+            }
+            else {
+                return $false
+            }
+        }
+        #Key key without value
+        else {
+            $RegKeyCheck = Get-ItemProperty -Path $Path -ErrorAction Stop | Select-Object -ExpandProperty $Name -ErrorAction Stop
+            if ($null -eq $RegKeyCheck) {
+                #if the Key Check returns null then it probably means that the key does not exist.
+                return $false
+            }
+            else {
+                return $true
+            }
+        }
+    }
+    catch {
+        return $false
+    }
+}
+
+Write-Output "Check if the computer is an RDS Server"
+
+$TSMode = $string = Get-WmiObject -Namespace "root\CIMV2\TerminalServices" -Class "Win32_TerminalServiceSetting"  | select -ExpandProperty TerminalServerMode
+
+if ($TSMode -eq '1') {
+    #Check to make sure UPDs are enabled
+    if((Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Terminal Server\ClusterSettings' -Name UvhdEnabled -Value 1) -or
+	(Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -ValueData 1)){
+        schtasks /create /RU SYSTEM /ST 04:00 /SC DAILY /TN "$ScheduledTaskName" /TR "%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass C:\BIN\DelProf2\Remove-LocalUPDProfiles.ps1" /F
+        Write-Host 'Make sure that C:\BIN\DelProf2\Remove-LocalUPDProfiles.ps1 and C:\BIN\DelProf2\DelProf2.exe exist' -BackgroundColor Yellow -ForegroundColor Black
+    }
+    else {
+        Write-Output "UPDs are not enabled. The script will not run"
+    }
+}
+else {
+    Write-Output "The computer is not an RDS Server. The script will not run"
+}
