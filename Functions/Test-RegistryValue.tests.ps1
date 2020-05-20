8724383a5a2875c69d319a799625600f289e65ad
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-05-20 17:07:13 -0400

    formatting

--- Functions/Test-RegistryValue.tests.ps1
+++ Functions/Test-RegistryValue.tests.ps1
@@ -7,6 +7,7 @@ $script_dir = Split-Path -Parent $MyInvocation.MyCommand.Path
 #Load the function
 . "$script_dir\Test-RegistryValue.ps1"
 
+
 $TestData = @(
     @{
         'TestName'       = 'Key that Exists';
@@ -65,10 +66,20 @@ $TestData = @(
 
 Describe 'Test-RegistryValue Function' {
 
-
     It "<TestName>" -TestCases $TestData {
         param ($RegPath, $RegName, $RegValueData, $ExpectedResult)
         Test-RegistryValue -Path $RegPath -Name $RegName -ValueData $RegValueData | Should -Be $ExpectedResult
+    }
+
+    Context 'Check for Null Key' {
+        Mock Get-ItemProperty {
+            return $null
+        }
+
+        It 'Test if finding a $null key returns false' {
+            Test-RegistryValue -Path 'HKEY_LOCAL_MACHINE\FakePath' -Name 'FakeName' | should -Be $false
+        }
+
     }
 
 }
