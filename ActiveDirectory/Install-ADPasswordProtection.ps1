4d9d218204413fbbc3d6efa4a03db3abef105f23
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-08-11 16:41:37 -0400

    Working Working without downloading the DB files directly

--- ActiveDirectory/Install-ADPasswordProtection.ps1
+++ ActiveDirectory/Install-ADPasswordProtection.ps1
@@ -1,78 +1,352 @@
-
+#Requires -Version 5 -RunAsAdministrator
 #https://haveibeenpwned.com/Passwords
+
+
 $HIBPHashesLink = 'https://downloads.pwnedpasswords.com/passwords/pwned-passwords-ntlm-ordered-by-hash-v6.7z'
 $HIBPHashesCompressedFile = 'C:\Temp\pwned-passwords-ntlm-ordered-by-hash.7z'
 $HIBPHashesExtractDir = 'C:\Temp\HIBP'
 $PasswordProtectionMSIFile = 'C:\Windows\Temp\Lithnet.ActiveDirectory.PasswordProtection.msi'
+$GPOPath = 'C:\Windows\Temp\PasswordProtection.zip'
+$LogDirectory = 'C:\Windows\Temp\PasswordProtection.log'
 
-#Clean up any old files
+function Write-Log {
+    <#
+    .Synopsis
+    Write-Log writes a message to a specified log file with the current time stamp.
+    .DESCRIPTION
+    The Write-Log function is designed to add logging capability to other scripts.
+    In addition to writing output and/or verbose you can write to a log file for
+    later debugging.
+    .NOTES
+    Created by: Jason Wasser @wasserja
+    Modified: 11/24/2015 09:30:19 AM
+
+    Changelog:
+        * Code simplification and clarification - thanks to @juneb_get_help
+        * Added documentation.
+        * Renamed LogPath parameter to Path to keep it standard - thanks to @JeffHicks
+        * Revised the Force switch to work as it should - thanks to @JeffHicks
+
+    To Do:
+        * Add error handling if trying to create a log file in a inaccessible location.
+        * Add ability to write $Message to $Verbose or $Error pipelines to eliminate
+        duplicates.
+    .PARAMETER Message
+    Message is the content that you wish to add to the log file.
+    .PARAMETER Path
+    The path to the log file to which you would like to write. By default the function will
+    create the path and file if it does not exist.
+    .PARAMETER Level
+    Specify the criticality of the log information being written to the log (i.e. Error, Warning, Informational)
+    .PARAMETER NoClobber
+    Use NoClobber if you do not wish to overwrite an existing file.
+    .EXAMPLE
+    Write-Log -Message 'Log message'
+    Writes the message to c:\Logs\PowerShellLog.log.
+    .EXAMPLE
+    Write-Log -Message 'Restarting Server.' -Path c:\Logs\Scriptoutput.log
+    Writes the content to the specified log file and creates the path and file specified.
+    .EXAMPLE
+    Write-Log -Message 'Folder does not exist.' -Path c:\Logs\Script.log -Level Error
+    Writes the message to the specified log file as an error message, and writes the message to the error pipeline.
+    .LINK
+    https://gallery.technet.microsoft.com/scriptcenter/Write-Log-PowerShell-999c32d0
+    #>
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("LogContent")]
+        [string]$Message,
+
+        [Parameter(Mandatory = $true)]
+        [Alias('LogPath')]
+        [string]$Path,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Error", "Warn", "Info")]
+        [string]$Level = "Info",
+
+        [Parameter(Mandatory = $false)]
+        [switch]$NoClobber,
+
+        [Parameter(Mandatory = $false)]
+        [switch]$DaiyMode
+    )
+
+    Begin {
+        # Set VerbosePreference to Continue so that verbose messages are displayed.
+        $VerbosePreference = 'Continue'
+        if ($DaiyMode) {
+            $Path = $Path.Replace('.', "-$(Get-Date -UFormat "%Y%m%d").")
+        }
+    }
+    Process {
+        # If the file already exists and NoClobber was specified, do not write to the log.
+        if ((Test-Path $Path) -AND $NoClobber) {
+            Write-Error "Log file $Path already exists, and you specified NoClobber. Either delete the file or specify a different name."
+            Return
+        }
+
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path.
+        elseif (!(Test-Path $Path)) {
+            Write-Verbose "Creating $Path."
+            New-Item $Path -Force -ItemType File
+        }
+
+        else {
+            # Nothing to see here yet.
+        }
+
+        # Format Date for our Log File
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+        # Write message to error, warning, or verbose pipeline and specify $LevelText
+        switch ($Level) {
+            'Error' {
+                Write-Error $Message
+                $LevelText = 'ERROR:'
+            }
+            'Warn' {
+                Write-Warning $Message
+                $LevelText = 'WARNING:'
+            }
+            'Info' {
+                Write-Verbose $Message
+                $LevelText = 'INFO:'
+            }
+        }
+
+        # Write log entry to $Path
+        #try to write to the log file. Rety if it is locked
+        $StopWriteLogloop = $false
+        [int]$WriteLogRetrycount = "0"
+        do {
+            try {
+                "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append -ErrorAction Stop
+                $StopWriteLogloop = $true
+            }
+            catch {
+                if ($WriteLogRetrycount -gt 5) {
+                    $StopWriteLogloop = $true
+                }
+                else {
+                    Start-Sleep -Milliseconds 500
+                    $WriteLogRetrycount++
+                }
+            }
+        }While ($StopWriteLogloop -eq $false)
+    }
+    End {
+    }
+}
 Function Remove-OldFiles {
     $ItemsToDelete = @(
         $HIBPHashesCompressedFile,
-        $HIBPHashesExtractDir
+        $HIBPHashesExtractDir,
+        $GPOPath,
+        $GPOFolder
     )
+
+    Write-Log -Level Info -Path $LogDirectory -Message 'Deleting old files if they exist.'
 
     foreach ($item in $ItemsToDelete) {
         Remove-Item $item -Force -Recurse -ErrorAction SilentlyContinue
     }
 }
+Function Get-InstalledApplications {
+    $InstalledApplications = @()
+    $UninstallKeys = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+    Foreach ($SubKey in $UninstallKeys) {
 
+        $DisplayName = (Get-ItemProperty -Path "Registry::$($SubKey.Name)" -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
+        if ([string]::IsNullOrEmpty($DisplayName)) {
+        }
+        else {
+            $InstalledApplications += [PSCustomObject]@{
+                DisplayName = $DisplayName
+            }
+        }
+
+    }
+
+    Return $InstalledApplications
+}
+
+
+#Check if computer is a DC
+if ((Get-WmiObject Win32_ComputerSystem).domainrole -lt 4) {
+    Write-Log -Level Info -Path $LogDirectory -Message 'Computer is not a DC. Script will exit'
+    exit
+}
+
+#region Check For Existing components
+$GPOExistsWithCorrectSettings = $false
+$ADPasswordProtectionAlreadyInstalled = $false
+$HIBPDBFilesExist = $false
+
+#Check for GPO
+if (Get-GPO -Name 'Password Protection' -ErrorAction SilentlyContinue) {
+    [XML]$GPOReport = Get-GPO -Name 'Password Protection' -ErrorAction SilentlyContinue | Get-GPOReport -ReportType Xml
+
+    $GPOSetting = $GPOReport.GPO.Computer.ExtensionData.Extension.Policy | Where-Object { $_.Name -eq 'Reject passwords found in the compromised password store' }
+    if ($GPOSetting.State -eq 'Enabled') {
+        $GPOExistsWithCorrectSettings = $true
+        Write-Log -Level Info -Path $LogDirectory -Message "The GPO $($GPOReport.GPO.Name) will not be created since it already exists"
+    }
+}
+
+#Check for app installed
+if ((Get-InstalledApplications).displayname -contains 'Lithnet Password Protection for Active Directory') {
+    $ADPasswordProtectionAlreadyInstalled = $true
+    Write-Log -Level Info -Path $LogDirectory -Message "The Password Protection application is already installed on this computer"
+}
+
+#Check to see if DB files exist
+if (Test-Path -Path 'C:\Program Files\Lithnet\Active Directory Password Protection\Store\v3\p\FFFF.db') {
+    $HIBPDBFilesExist = $true
+    Write-Log -Level Info -Path $LogDirectory -Message "The HIBP DB files already exist in the default location"
+}
+#endregion Check For Existing components
+
+#Clean up any old files
 Remove-OldFiles
 
-if ((Get-PSDrive C).free -gt 30GB) {
-    #Download and install MSI
+if ((Get-PSDrive C).free -gt 25GB) {
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    (New-Object System.Net.WebClient).DownloadFile('https://github.com/lithnet/ad-password-protection/releases/latest/download/Lithnet.ActiveDirectory.PasswordProtection.msi', "$PasswordProtectionMSIFile")
-
-    msiexec.exe /i 'C:\Windows\Temp\Lithnet.ActiveDirectory.PasswordProtection.msi' /qn
 
     #Install 7zip module
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-    Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted
-    Install-Module -Name 7Zip4PowerShell -Force
+    if (-not (Get-Module -Name 7Zip4PowerShell -ListAvailable)) {
+        Write-Log -Level Info -Path $LogDirectory -Message 'Installing 7zip PS module'
+
+        try {
+            Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -ErrorAction Stop
+            Set-PSRepository -Name 'PSGallery' -SourceLocation "https://www.powershellgallery.com/api/v2" -InstallationPolicy Trusted -ErrorAction Stop
+            Install-Module -Name 7Zip4PowerShell -Force -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error -Path $LogDirectory -Message 'Ran into an issue installing the 7Zip4PowerShell module'
+            BREAK
+        }
+    }
 
     #Download HIBP Hashes
-    (New-Object System.Net.WebClient).DownloadFile("$HIBPHashesLink", "$HIBPHashesCompressedFile")
+    if (-not $HIBPDBFilesExist) {
+        Write-Log -Level Info -Path $LogDirectory -Message 'Downloading HIBP hashes'
+        New-Item -Path 'C:\Temp' -ItemType Directory -Force
 
-    #Extract HIBP Hashes
-    Expand-7Zip -ArchiveFileName $HIBPHashesCompressedFile -TargetPath $HIBPHashesExtractDir
+        (New-Object System.Net.WebClient).DownloadFile("$HIBPHashesLink", "$HIBPHashesCompressedFile")
 
-    Remove-Item $HIBPHashesCompressedFile -Force
+        #Extract HIBP Hashes
+        Write-Log -Level Info -Path $LogDirectory -Message 'Extracting HIBP hashes'
 
-    Import-Module LithnetPasswordProtection
-    Open-Store 'C:\Program Files\Lithnet\Active Directory Password Protection\Store'
-    Import-CompromisedPasswordHashes -Filename (Get-ChildItem -Path $HIBPHashesExtractDir).fullname
-
-    #region Copy ADM files to central store
-    if (Test-Path 'C:\Windows\SYSVOL') {
-        $SYSVOLPath = 'C:\Windows\SYSVOL'
-    }
-    elseif (Test-Path 'C:\Windows\SYSVOL_DFSR') {
-        $SYSVOLPath = 'C:\Windows\SYSVOL_DFSR'
-    }
-
-    $FilesToCopy = @(
-        'C:\Windows\PolicyDefinitions\lithnet.admx',
-        'C:\Windows\PolicyDefinitions\lithnet.activedirectory.passwordfilter.admx',
-        'C:\Windows\PolicyDefinitions\en-US\lithnet.activedirectory.passwordfilter.adml',
-        'C:\Windows\PolicyDefinitions\en-US\lithnet.adml'
-    )
-
-    Foreach ($File in $FilesToCopy) {
-        if($File -like '*.adml'){
-            $Destination = "$($SYSVOLPath)\domain\Policies\PolicyDefinitions\en-US"
+        try {
+            Expand-7Zip -ArchiveFileName $HIBPHashesCompressedFile -TargetPath $HIBPHashesExtractDir
         }
-        elseif ($File -like '*.admx') {
-            $Destination = "$($SYSVOLPath)\domain\Policies\PolicyDefinitions"
+        catch {
+            Write-Log -Level Error -Path $LogDirectory -Message "Ran into an issue extracting the 7Z file $HIBPHashesCompressedFile"
         }
 
-        Move-Item -Path $File -Destination $Destination -Force
+        Remove-Item $HIBPHashesCompressedFile -Force -ErrorAction SilentlyContinue
     }
-    #endregion Copy ADM files to central store
 
+
+    #Download and install MSI
+    if (-not $ADPasswordProtectionAlreadyInstalled) {
+        (New-Object System.Net.WebClient).DownloadFile('https://github.com/lithnet/ad-password-protection/releases/latest/download/Lithnet.ActiveDirectory.PasswordProtection.msi', "$PasswordProtectionMSIFile")
+
+        Write-Log -Level Info -Path $LogDirectory -Message 'Installing Password Protection MSI'
+        Start-Process msiexec.exe -Wait -ArgumentList "/i $($PasswordProtectionMSIFile) /qn" -PassThru
+
+        Write-Log -Level Info -Path $LogDirectory -Message "The Password Protection application has been installed. Restart the computer for the change to take effect."
+    }
+
+    #region Convert hashes to DB files
+    if (-not $HIBPDBFilesExist) {
+        #This will take 40min+
+        Write-Log -Level Info -Path $LogDirectory -Message 'Converting HIBP hashes to DB files. This will take 40+ mins'
+
+        try {
+            Import-Module LithnetPasswordProtection
+            Open-Store 'C:\Program Files\Lithnet\Active Directory Password Protection\Store' -ErrorAction Stop
+            Import-CompromisedPasswordHashes -Filename (Get-ChildItem -Path $HIBPHashesExtractDir).fullname -ErrorAction Stop
+        }
+        catch {
+            Write-Log -Level Error -Path $LogDirectory -Message "Ran into an issue importing hashes from $HIBPHashesExtractDir"
+            BREAK
+        }
+    }
+    #endregion Convert hashes to DB files
+
+    #region Import GPO
+    if (-not $GPOExistsWithCorrectSettings) {
+        if ((Get-WmiObject Win32_ComputerSystem).domainrole -eq 5) {
+            #region Copy ADM files to central store
+            if (Test-Path 'C:\Windows\SYSVOL') {
+                $SYSVOLPath = 'C:\Windows\SYSVOL'
+            }
+            elseif (Test-Path 'C:\Windows\SYSVOL_DFSR') {
+                $SYSVOLPath = 'C:\Windows\SYSVOL_DFSR'
+            }
+
+            $FilesToCopy = @(
+                'C:\Windows\PolicyDefinitions\lithnet.admx',
+                'C:\Windows\PolicyDefinitions\lithnet.activedirectory.passwordfilter.admx',
+                'C:\Windows\PolicyDefinitions\en-US\lithnet.activedirectory.passwordfilter.adml',
+                'C:\Windows\PolicyDefinitions\en-US\lithnet.adml'
+            )
+
+            Foreach ($File in $FilesToCopy) {
+                if ($File -like '*.adml') {
+                    $Destination = "$($SYSVOLPath)\domain\Policies\PolicyDefinitions\en-US"
+                }
+                elseif ($File -like '*.admx') {
+                    $Destination = "$($SYSVOLPath)\domain\Policies\PolicyDefinitions"
+                }
+
+                if (-not (Test-Path $Destination)) {
+                    Write-Log -Level Info -Path $LogDirectory -Message "Moving $File to $Destination"
+
+                    Move-Item -Path $File -Destination $Destination -Force
+                }
+            }
+            #endregion Copy ADM files to central store
+
+            Write-Log -Level Info -Path $LogDirectory -Message 'Downloading GPO'
+
+            (New-Object System.Net.WebClient).DownloadFile('https://github.com/CompassMSP/PublicScripts/raw/master/ActiveDirectory/GPOBackups/Password%20Protection.zip', "$GPOPath")
+
+            $GPOFolder = $GPOPath.Replace('.zip', '')
+
+            Expand-Archive -LiteralPath  $GPOPath -DestinationPath $GPOFolder -Force
+
+            $GPOBackupFolder = (Get-ChildItem $GPOFolder).FullName
+
+            #Get the Name of the GPO from the content of the XML
+            $GPOReportPath = Get-ChildItem $GPOFolder -Recurse | Where-Object name -EQ gpreport.xml
+            [XML]$GPOReportXML = Get-Content -Path $GPOReportPath.FullName
+            [string]$GPOBackupName = $GPOReportXML.GPO.Name
+
+            try {
+                New-GPO -Name 'Password Protection' -ErrorAction Stop
+                Import-GPO -Path $GPOBackupFolder -TargetName 'Password Protection' -BackupGpoName $GPOBackupName -ErrorAction Stop
+
+                New-GPLink -Name 'Password Protection' -Target (Get-ADDomain).DistinguishedName -LinkEnabled Yes -ErrorAction Stop
+            }
+            catch {
+                Write-Log -Level Error -Path $LogDirectory -Message "Ran into an issue importing the GPO from $GPOBackupFolder"
+            }
+        }
+        else {
+            Write-Log -Level Info -Path $LogDirectory -Message 'Computer is not the PDC. GPO will not be imported'
+        }
+    }
+    #endregion Import GPO
 }
 else {
-    Write-Error 'Not enough free space on the disk'
+    Write-Log -Level Error -Path $LogDirectory -Message 'Not enough free space on the C drive. At least 30GB required.'
 }
 
 #Cleanup
