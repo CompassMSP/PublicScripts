1a48e12cc2488dc117632ef56bb9e8334390f95d
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-08-12 09:10:37 -0400

    Initial commit

---
+++ Functions/Get-InstalledApplications.ps1
@@ -0,0 +1,18 @@
+Function Get-InstalledApplications {
+    $InstalledApplications = @()
+    $UninstallKeys = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+    Foreach ($SubKey in $UninstallKeys) {
+
+        $DisplayName = (Get-ItemProperty -Path "Registry::$($SubKey.Name)" -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
+        if ([string]::IsNullOrEmpty($DisplayName)) {
+        }
+        else{
+            $InstalledApplications += [PSCustomObject]@{
+                DisplayName = $DisplayName
+            }
+        }
+
+    }
+
+    Return $InstalledApplications
+}
