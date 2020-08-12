54341f82c3f60e67329207b7c1858b497befc316
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-08-12 09:05:07 -0400

    converted to function

--- ActiveDirectory/Invoke-ADPasswordAudit.ps1
+++ ActiveDirectory/Invoke-ADPasswordAudit.ps1
@@ -5,51 +5,52 @@ Lithnet AD Password Protection must be enabled and configured on the domain. The
 
 Andy Morales
 #>
+Function Invoke-ADPasswordAudit {
+    #Requires -Modules LithnetPasswordProtection
 
-#Requires -Modules LithnetPasswordProtection
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$NotificationEmail,
 
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory = $true)]
-    [string]$NotificationEmail,
+        [Parameter(Mandatory = $true)]
+        [string]$FromEmail,
 
-    [Parameter(Mandatory = $true)]
-    [string]$FromEmail,
+        [Parameter(Mandatory = $true)]
+        [string]$SMTPRelay
+    )
 
-    [Parameter(Mandatory = $true)]
-    [string]$SMTPRelay
-)
+    Import-Module LithnetPasswordProtection
 
-Import-Module LithnetPasswordProtection
+    $CompromisedUsers = @()
 
-$CompromisedUsers = @()
+    $AllActiveADUsers = Get-ADUser -Filter 'enabled -eq "true"' -Properties PwdLastSet, lastLogonTimeStamp, mail
 
-$AllActiveADUsers = Get-ADUser -Filter 'enabled -eq "true"' -Properties PwdLastSet, lastLogonTimeStamp, mail
-
-#Identify any compromised users
-Foreach ($user in $AllActiveADUsers) {
-    if (Test-IsADUserPasswordCompromised -AccountName $user.SamAccountName) {
-        $CompromisedUsers += [PSCustomObject]@{
-            Name              = $user.Name
-            SamAccountName    = $user.SamAccountName
-            Mail              = $user.mail
-            UserPrincipalName = $user.UserPrincipalName
-            PwdLastSet        = [DateTime]::FromFileTimeUtc($user.PwdLastSet).ToShortDateString()
+    #Identify any compromised users
+    Foreach ($user in $AllActiveADUsers) {
+        if (Test-IsADUserPasswordCompromised -AccountName $user.SamAccountName) {
+            $CompromisedUsers += [PSCustomObject]@{
+                Name              = $user.Name
+                SamAccountName    = $user.SamAccountName
+                Mail              = $user.mail
+                UserPrincipalName = $user.UserPrincipalName
+                PwdLastSet        = [DateTime]::FromFileTimeUtc($user.PwdLastSet).ToShortDateString()
+            }
         }
     }
-}
 
-#Send message if any compromised users are found
-If ($CompromisedUsers.count -gt 0) {
-    $EmailBody = $CompromisedUsers | ConvertTo-Html -Fragment -As Table | Out-String
+    #Send message if any compromised users are found
+    If ($CompromisedUsers.count -gt 0) {
+        $EmailBody = $CompromisedUsers | ConvertTo-Html -Fragment -As Table | Out-String
 
-    $SendMailMessageParams = @{
-        To         = $NotificationEmail
-        From       = $FromEmail
-        Subject    = "Bad passwords found on domain $((Get-WmiObject Win32_ComputerSystem).Domain)"
-        BodyAsHtml = $EmailBody
-        SmtpServer = $SMTPRelay
+        $SendMailMessageParams = @{
+            To         = $NotificationEmail
+            From       = $FromEmail
+            Subject    = "Bad passwords found on domain $((Get-WmiObject Win32_ComputerSystem).Domain)"
+            BodyAsHtml = $EmailBody
+            SmtpServer = $SMTPRelay
+        }
+
+        Send-MailMessage @SendMailMessageParams
     }
-
-    Send-MailMessage @SendMailMessageParams
 }
