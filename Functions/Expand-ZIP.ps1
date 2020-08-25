873c57594814f1e62026065aead65bbfc9100736
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-08-25 11:20:48 -0400

    Initial commit

---
+++ Functions/Expand-ZIP.ps1
@@ -0,0 +1,22 @@
+function Expand-ZIP {
+    <#
+    Extracts a ZIP file to a directory. The contents of the destination will be deleted if they already exist.
+
+    Andy Morales
+    #>
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory = $true)]
+        [String]$ZipFile,
+
+        [parameter(Mandatory = $true)]
+        [String]$OutPath
+    )
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+    if (Test-Path -Path $OutPath){
+        Remove-Item $OutPath -Recurse -Force
+    }
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($ZipFile, $OutPath)
+}
