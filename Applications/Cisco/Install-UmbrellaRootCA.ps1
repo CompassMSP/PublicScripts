501f1f87a66a1ea3ee0ca6315ddaf0ffdb33db4c
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-09-24 11:02:04 -0400

    added firefox setting

--- Applications/Cisco/Install-UmbrellaRootCA.ps1
+++ Applications/Cisco/Install-UmbrellaRootCA.ps1
@@ -1,5 +1,5 @@
 <#
-This script installs the Cisco Umbrella root CA certificate on the computer store
+This script installs the Cisco Umbrella root CA certificate on the computer store.
 
 Andy Morales
 #>
@@ -8,3 +8,6 @@ Andy Morales
 (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/CompassMSP/PublicScripts/master/Applications/Cisco/Cisco_Umbrella_Root_CA.cer', 'C:\Windows\Temp\Cisco_Umbrella_Root_CA.cer')
 
 CERTUTIL -addStore -enterprise -f -v root "C:\Windows\Temp\Cisco_Umbrella_Root_CA.cer"
+
+#Force Firefox to use local cert store
+REG  add  "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Mozilla\Firefox\Certificates"  /v  "ImportEnterpriseRoots" /t  REG_DWORD  /d  1 /f
