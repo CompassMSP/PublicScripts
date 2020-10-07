7f6d2eb491794c3d53df37fa1298fe7d90754926
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-10-07 17:01:13 -0400

    initial commit

---
+++ Security/VulnFixes/Invoke-LSAAudit.ps1
@@ -0,0 +1,62 @@
+<#
+This script will check the event log to make sure that LSA protection can be enabled.
+
+Andy Morales
+#>
+Function Invoke-LSAAudit {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string]$NotificationEmail,
+
+        [Parameter(Mandatory = $true)]
+        [string]$FromEmail,
+
+        [Parameter(Mandatory = $true)]
+        [string]$SMTPRelay
+    )
+
+    $WinEventHashTable = @{
+        ProviderName = 'Microsoft-Windows-CodeIntegrity';
+        Id           = '3065', '3066'
+    }
+
+    $Events = Get-WinEvent -FilterHashtable  $WinEventHashTable
+
+    if ($Events.count -gt 0) {
+        #Filter out known events
+        $FilteredEvents = @()
+
+        $EventsToExclude = @(
+            <#'CyMemDef64.dll',#>
+            'Signature information for another event. Match using the Correlation Id.'
+        )
+
+        Foreach ($Event in $Events) {
+            if ($Event.Message -notMatch ($EventsToExclude -join '|')) {
+                $FilteredEvents += $Event
+            }
+        }
+
+        if ($FilteredEvents.Count -gt 0) {
+            $EmailBody = $FilteredEvents[0..10] | ConvertTo-Html -Fragment -As Table | Out-String
+
+            $SendMailMessageParams = @{
+                To         = $NotificationEmail
+                From       = $FromEmail
+                Subject    = "Found LSA issue on $($ENV:COMPUTERNAME)"
+                Body       = $EmailBody
+                BodyAsHtml = $true
+                SmtpServer = $SMTPRelay
+            }
+
+            Send-MailMessage @SendMailMessageParams
+        }
+        else {
+            Write-Output 'No Events Found'
+        }
+    }
+    else {
+        Write-Output 'No events found'
+    }
+}
