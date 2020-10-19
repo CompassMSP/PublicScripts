ddb2958e5289e972fb136cbee4511790532004b3
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-10-19 15:35:29 -0400

    added domain controller detection

--- Applications/Cisco/Install-UmbrellaRoamingClient.ps1
+++ Applications/Cisco/Install-UmbrellaRoamingClient.ps1
@@ -19,22 +19,28 @@ Function Install-UmbrellaRoamingClient {
         [switch]$HIDE_UI
     )
 
-    $MSIDestination = 'C:\Windows\Temp\UmbrellaSetup.msi'
+    #Check to see if the computer is a domain controller
+    if ((Get-WmiObject Win32_ComputerSystem).DomainRole -le 3) {
+        $MSIDestination = 'C:\Windows\Temp\UmbrellaSetup.msi'
 
-    #Download MSI
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+        #Download MSI
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-    (New-Object System.Net.WebClient).DownloadFile('http://shared.opendns.com/roaming/enterprise/release/win/production/Setup.msi', "$MSIDestination")
+        (New-Object System.Net.WebClient).DownloadFile('http://shared.opendns.com/roaming/enterprise/release/win/production/Setup.msi', "$MSIDestination")
 
-    #Install the application
-    $MSIParams = "/i $($MSIDestination) /qn ORG_ID=$($ORG_ID) ORG_FINGERPRINT=$($ORG_FINGERPRINT) USER_ID=$($USER_ID)"
+        #Install the application
+        $MSIParams = "/i $($MSIDestination) /qn ORG_ID=$($ORG_ID) ORG_FINGERPRINT=$($ORG_FINGERPRINT) USER_ID=$($USER_ID)"
 
-    if ($HIDE_UI){
-        $MSIParams += 'HIDE_UI=1'
+        if ($HIDE_UI) {
+            $MSIParams += 'HIDE_UI=1'
+        }
+
+        Start-Process msiexec.exe -Wait -ArgumentList $MSIParams -PassThru
+
+        #Install root CA
+        Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/CompassMSP/PublicScripts/master/Applications/Cisco/Install-UmbrellaRootCA.ps1')
     }
-
-    Start-Process msiexec.exe -Wait -ArgumentList $MSIParams -PassThru
-
-    #Install root CA
-    Invoke-Expression(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/CompassMSP/PublicScripts/master/Applications/Cisco/Install-UmbrellaRootCA.ps1')
+    else {
+        Write-Output 'Computer is a domain controller. The script will exit.'
+    }
 }
