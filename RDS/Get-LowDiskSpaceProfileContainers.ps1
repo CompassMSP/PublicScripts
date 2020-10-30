fad96646bf6ffab325cd3d5d62db0e81c7a6939f
andy2002a <39770126+andy2002a@users.noreply.github.com>
2020-10-30 13:45:11 -0400

    added profile share display

--- RDS/Get-LowDiskSpaceProfileContainers.ps1
+++ RDS/Get-LowDiskSpaceProfileContainers.ps1
@@ -70,9 +70,12 @@ function Test-RegistryValue {
 }
 
 #Check to see if FSL or UPDs are enabled
-If (
-    (Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -ValueData '1') -or
-    (Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Terminal Server\ClusterSettings' -Name 'UvhdEnabled' -ValueData '1')){
+
+$UPDEnabled = Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Terminal Server\ClusterSettings' -Name 'UvhdEnabled' -ValueData '1'
+$FSLProfileEnabled = Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles' -Name 'Enabled' -ValueData '1'
+$FSLODFCEnabled = Test-RegistryValue -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\FSLogix\ODFC' -Name 'Enabled' -ValueData '1'
+
+If ($UPDEnabled -or $FSLProfileEnabled -or $UPDEnabled) {
 
     $AttachedProfileDisks = Get-CimInstance -Class win32_volume -Filter "Label like 'O365-%' OR Label like 'Profile-%' OR Label like 'User Disk'"
 
@@ -90,10 +93,22 @@ If (
             #FSLogix
             elseif (($Disk.Label -like 'Profile-*') -or ($Disk.Label -like 'O365-*')) {
                 $AllLowSpaceDisks += $Disk.label.ToString()
-
             }
         }
-        Return "LowSpaceProfile: $Env:COMPUTERNAME $($AllLowSpaceDisks -join ',')"
+
+        $Paths = @()
+
+        if ($UPDEnabled){
+            $Paths += "UPD path: " + (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Terminal Server\ClusterSettings' -Name UvhdShareUrl).UvhdShareUrl
+        }
+        if($FSLProfileEnabled){
+            $Paths += "FSL Profile path: " + (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\FSLogix\Profiles' -Name VHDLocations).VHDLocations
+        }
+        if ($FSLODFCEnabled) {
+            $Paths += "FSL ODFC path: " + (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\FSLogix\ODFC' -Name VHDLocations).VHDLocations
+        }
+
+        Return "LowSpaceProfile: $Env:COMPUTERNAME $($AllLowSpaceDisks -join ',')`r`n$($Paths -join [Environment]::NewLine)"
     }
 
 }
