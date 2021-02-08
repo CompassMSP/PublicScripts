091c1789988d913df5a1e2f52f99601fc1be110c
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-02-08 14:31:06 -0500

    updated x86 check

--- Functions/Get-InstalledApplications.ps1
+++ Functions/Get-InstalledApplications.ps1
@@ -1,17 +1,23 @@
 Function Get-InstalledApplications {
     $InstalledApplications = @()
     $UninstallKeys = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+
+    #Check for x86 software on x64 Windows
+    if (Test-Path -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') {
+        $UninstallKeys += Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    }
+
     Foreach ($SubKey in $UninstallKeys) {
 
         $DisplayName = (Get-ItemProperty -Path "Registry::$($SubKey.Name)" -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
         if ([string]::IsNullOrEmpty($DisplayName)) {
         }
-        else{
+        else {
             $InstalledApplications += [PSCustomObject]@{
                 DisplayName = $DisplayName
             }
         }
 
     }
-    Return $InstalledApplications
+    Return $InstalledApplications | Sort-Object displayname
 }
