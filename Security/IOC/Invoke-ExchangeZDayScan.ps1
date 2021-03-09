a352b50c76dc9f59bb0aab2e5bbb9eb9af647d06
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-03-09 09:16:36 -0500

    added zip exclusions

--- Security/IOC/Invoke-ExchangeZDayScan.ps1
+++ Security/IOC/Invoke-ExchangeZDayScan.ps1
@@ -14,7 +14,7 @@ Function Write-Log {
 }
 
 #Check to see if Exchange 2013+ is installed
-if ($env:exchangeinstallpath) {
+if ($env:exchangeInstallPath) {
 
     $errorFound = 0
 
@@ -146,7 +146,7 @@ if ($env:exchangeinstallpath) {
         Write-Log "No suspicious lsass dumps found."
     }
 
-    $zipFiles = @(Get-ChildItem -Recurse -Path "$env:ProgramData" -ErrorAction SilentlyContinue | Where-Object { $_.Extension -match ".7z|.zip|.rar" } | Where-Object { $_.FullName -notMatch 'Kaseya|LTDatabase|VMware Tools|LabTech' })
+    $zipFiles = @(Get-ChildItem -Recurse -Path "$env:ProgramData" -ErrorAction SilentlyContinue | Where-Object { $_.Extension -match ".7z|.zip|.rar" } | Where-Object { $_.FullName -notMatch 'Kaseya|LTDatabase|VMware Tools|LabTech|chocolatey|Norton|Symantec' })
 
     if ($zipFiles.Count -gt 0) {
         Write-Log "`r`nZipped files found in $env:ProgramData, please verify these are expected:"
