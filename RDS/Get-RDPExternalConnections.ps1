85c25139acafb4f2a57339da9c0d4e000b3f87c2
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-03-16 11:54:53 -0400

    initiall commit

---
+++ RDS/Get-RDPExternalConnections.ps1
@@ -0,0 +1,58 @@
+Function Get-InstalledApplications {
+    $InstalledApplications = @()
+    $UninstallKeys = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall'
+
+    #Check for x86 software on x64 Windows
+    if (Test-Path -Path 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall') {
+        $UninstallKeys += Get-ChildItem 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+    }
+
+    Foreach ($SubKey in $UninstallKeys) {
+
+        $DisplayName = (Get-ItemProperty -Path "Registry::$($SubKey.Name)" -Name DisplayName -ErrorAction SilentlyContinue).DisplayName
+        if ([string]::IsNullOrEmpty($DisplayName)) {
+        }
+        else {
+            $InstalledApplications += [PSCustomObject]@{
+                DisplayName = $DisplayName
+            }
+        }
+
+    }
+    Return $InstalledApplications | Sort-Object displayName
+}
+
+#Don't do anything if DUO is installed
+if (Get-InstalledApplications -Contains 'Duo Authentication for Windows Logon x64'){
+    Write-Output 'DUO installed'
+}
+else{
+    $Events = Get-WinEvent -LogName 'Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational'
+
+    #Filter by event ID
+    $FilteredEvents = @()
+
+    foreach ($evt in $events) {
+        if ($evt.id -eq '1149') {
+            $FilteredEvents += $evt
+        }
+    }
+
+    #Find events that contain public IPs
+    $ExternalEvents = @()
+
+    $rfc1918regex = '(192\.168\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5]))|(172\.([1][6-9]|[2][0-9]|[3][0-1])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5]))|(10\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5]))|(127\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5])\.([0-9]|[0-9][0-9]|[0-2][0-5][0-5]))'
+
+    foreach ($fEvent in $FilteredEvents) {
+        if ($fEvent.Message -notmatch $rfc1918regex) {
+            $ExternalEvents += $fEvent
+        }
+    }
+
+    if($ExternalEvents.Count -gt 0){
+        Write-Output "External RDP events found on $env:COMPUTERNAME"
+    }
+    else{
+        Write-Output 'No events found'
+    }
+}
