3065978e2e8c4e92087352c22250970ebfd5f8a9
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-04-29 15:02:15 -0400

    lowered threshold to test

--- Security/Invoke-AutorunsVtScan.ps1
+++ Security/Invoke-AutorunsVtScan.ps1
@@ -7,7 +7,7 @@ Andy Morales
 [CmdletBinding()]
 param (
     [parameter(Mandatory = $false)]
-    [String]$VtThresholdPercent = .15
+    [String]$VtThresholdPercent = .01
 )
 
 $AutoRunsFolder = 'C:\windows\temp\autoruns'
