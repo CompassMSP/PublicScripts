5de0aae80f7f96f8e37ad37ad7d960c87020494d
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-05-13 09:17:08 -0400

    fixed foreach loop logic

--- Security/Add-RULangToAllYsers.ps1
+++ Security/Add-RULangToAllYsers.ps1
@@ -4,32 +4,33 @@ This script is intended to help prevent ransomware attacks where it does not inf
 
 This will not replace the default, but rather add a second keyboard.
 
-The script will run through all the currently signed in users and add RU as second keyboard. Users will need to sign out for the change to take effect. Running the script repeatedly is recomended.
+The script will run through all the currently signed in users and add RU as second keyboard. Users will need to sign out for the change to take effect. Running the script repeatedly is recommended.
 
 Andy Morales
 #>
 
+$NewLanguageCode = '00000419'
+
 #only get reg keys belonging to signed in users
-$UserKeys = Get-ChildItem -Path registry::HKEY_USERS | Where-Object {$_.name.Length -eq 57}
+$UserKeys = Get-ChildItem -Path registry::HKEY_USERS | Where-Object { $_.name.Length -eq 57 }
 
-
-Foreach ($User in $UserKeys){
+:userKeys Foreach ($User in $UserKeys) {
 
     $CurrentLangs = Get-Item -Path "registry::$($user.name)\Keyboard Layout\Preload" | Select-Object -ExpandProperty Property | Where-Object { $_.Length -eq 1 }
 
-    Foreach ($lang in $CurrentLangs){
-        if((Get-ItemProperty -path "registry::$($user.name)\Keyboard Layout\Preload" -name $lang).$lang -eq '00000419'){
+    Foreach ($lang in $CurrentLangs) {
+        if ((Get-ItemProperty -Path "registry::$($user.name)\Keyboard Layout\Preload" -Name $lang).$lang -eq $NewLanguageCode) {
 
-            Write-Output 'Lang is already installed. Exit script'
-            Exit
+            Write-Output "Lang is already installed for $($user.name)."
+            Continue userKeys
 
         }
     }
 
-    $HighestLangNumber = ($CurrentLangs| Measure-Object -Maximum).Maximum
+    $HighestLangNumber = ($CurrentLangs | Measure-Object -Maximum).Maximum
 
     $NewLangNumber = $HighestLangNumber + 1
 
-    Set-ItemProperty -Path "registry::$($user.name)\Keyboard Layout\Preload" -Name $NewLangNumber -Value '00000419' -Type String
+    Set-ItemProperty -Path "registry::$($user.name)\Keyboard Layout\Preload" -Name $NewLangNumber -Value $NewLanguageCode -Type String
 
 }
