9a10bb9dc72fbfea46175437d4bd75d0e9e6ea9d
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-05-24 10:49:41 -0400

    initial commit

---
+++ Security/IOC/Get-WmiSubscriptions.ps1
@@ -0,0 +1,34 @@
+<#
+This script searches for WMI subscriptions that might contain entires used for persistence.
+
+.LINK
+https://pentestlab.blog/2020/01/21/persistence-wmi-event-subscription/
+#>
+
+$ClassNames = @(
+    '__EventFilter',
+    '__FilterToConsumerBinding',
+    '__EventConsumer',
+    'CommandLineEventConsumer'
+)
+
+$WmiSubs = @()
+
+Foreach ($class in $ClassNames) {
+    $WmiSubs += Get-WmiObject -Namespace root\Subscription -Class $class
+}
+
+$SuspiciousSubFound = $false
+$SuspiciousSubs = @()
+
+foreach ($sub in $WmiSubs) {
+    if ($sub.CommandLineTemplate -like '*powershell*') {
+        $SuspiciousSubFound = $true
+        $SuspiciousSubs += $sub
+    }
+}
+
+if ($SuspiciousSubFound) {
+    Write-Output 'SuspiciousSubsFound'
+    Write-Output $SuspiciousSubs | Select-Object Name, CommandLineTemplate
+}
