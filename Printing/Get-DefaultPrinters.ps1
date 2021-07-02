bd6cb3cf51c2e3a72090d877da5e7cff4adbfcd0
andy2002a <39770126+andy2002a@users.noreply.github.com>
2021-07-02 10:25:41 -0400

    Initial commit

---
+++ Printing/Get-DefaultPrinters.ps1
@@ -0,0 +1,42 @@
+<#
+This script enumerates all the printers on a computer to see if only the default printers exist.
+
+.LINK
+https://github.com/gtworek/PSBits/blob/master/Misc/StopAndDisableDefaultSpoolers.ps1
+
+Andy Morales
+#>
+
+$service = Get-Service -Name Spooler -ErrorAction SilentlyContinue
+
+if (!$service) {
+    #Cannot connect to Spooler Service
+    EXIT
+}
+
+if ($service.Status -ne "Running") {
+    Write-Output "SpoolerNotRunning"
+    EXIT
+}
+
+$printers = Get-WmiObject -Class Win32_printer
+
+if (!$printers) {
+    Write-Output "CannotEnumeratePrinters"
+    EXIT
+}
+
+$OnlyDefaultPrintersExist = $true
+
+foreach ($DriverName in ($printers.DriverName)) {
+    if (($DriverName -notmatch 'Microsoft XPS Document Writer') -and ($DriverName -notmatch 'Microsoft Print To PDF')) {
+        $OnlyDefaultPrintersExist = $false
+    }
+}
+
+if ($OnlyDefaultPrintersExist) {
+    Write-Output "OnlyDefaultPrinters"
+}
+else {
+    Write-Output "NonDefaultPrintersFound"
+}
