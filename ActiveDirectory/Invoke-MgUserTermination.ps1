6fe049b2342440a81a636a2279126042fee4d485
tresnoface <ryanchrisw@gmail.com>
2022-07-06 11:50:04 -0400

    1.4 - Improved Readability and Group Exports

--- ActiveDirectory/Invoke-MgUserTermination.ps1
+++ ActiveDirectory/Invoke-MgUserTermination.ps1
@@ -11,7 +11,7 @@
 # 03-15-2022                    1.1         Added exports of groups and licenses
 # 06-27-2022                    1.2         Fixes for Remove-MgGroupMemberByRef and Revoke-MgUserSign
 # 06-28-2022                    1.3         Add removal of manager from disabled user and optimization changes
-#
+# 07-06-2022                    1.4         Improved readability and export for user groups
 #
 #********************************************************************************
 # Run from the Primary Domain Controller with AD Connect installed
@@ -35,7 +35,7 @@ try {
     $UserFromAD = Get-ADUser -Identity $User -Properties MemberOf -ErrorAction Stop
 }
 catch {
-    Write-Output "Could not find user $($User) in Active Directory"
+    Write-Host "Could not find user $($User) in Active Directory" -ForegroundColor Red -BackgroundColor Black
     exit
 }
 
@@ -55,45 +55,45 @@ if ($DisabledOUs.count -gt 0) {
     }
 }
 else {
-    Write-Output "Could not find disabled OU in Active Directory"
+    Write-Host "Could not find disabled OU in Active Directory" -ForegroundColor Red -BackgroundColor Black
     exit
 }
 #endregion pre-check
 
-Write-Output "Logging into Azure services. You should get 2 prompts."
+Write-Host "Logging into Azure services. You should get 2 prompts." -ForegroundColor Cyan -BackgroundColor Black
 
 Connect-ExchangeOnline
 Select-MgProfile Beta
 Connect-Graph -Scopes "Directory.ReadWrite.All", "User.ReadWrite.All", "Directory.AccessAsUser.All", "Group.ReadWrite.All", "GroupMember.Read.All"
 
-Write-Output "Attempting to find $($UserFromAD.UserPrincipalName) in Azure"
+Write-Host "Attempting to find $($UserFromAD.UserPrincipalName) in Azure" -ForegroundColor Cyan -BackgroundColor Black
 
 try {
     $365Mailbox = Get-Mailbox -Identity $UserFromAD.UserPrincipalName -ErrorAction Stop
     $MgUser = Get-MgUser -UserId $UserFromAD.UserPrincipalName -ErrorAction Stop
 }
 catch {
-    Write-Output "Could not find user $($UserFromAD.UserPrincipalName) in Azure"
+    Write-Host "Could not find user $($UserFromAD.UserPrincipalName) in Azure" -ForegroundColor Red -BackgroundColor Black
     exit
 }
 
-$Confirmation = Read-Host -Prompt "The user below will be disabled:`n
+$Confirmation = $(Write-Host "The user below will be disabled:`n
 Display Name = $($UserFromAD.Name)
 UserPrincipalName = $($UserFromAD.UserPrincipalName)
 Mailbox name =  $($365Mailbox.DisplayName)
 Azure name = $($MgUser.DisplayName)
 Destination OU = $($DestinationOU)`n
-(Y/N)`n"
+(Y/N)`n"  -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
 
 if ($Confirmation -ne 'y') {
-    Write-Output 'User did not enter "Y"'
+    Write-Host 'User did not enter "Y"' -ForegroundColor Red -BackgroundColor Black
     exit
 }
 
 #region ActiveDirectory
 
 #Modify the AD user account
-Write-Output "Performing Active Directory Steps"
+Write-Host "Performing Active Directory Steps" -ForegroundColor Cyan -BackgroundColor Black
 
 $SetADUserParams = @{
     Identity    = $UserFromAD.SamAccountName
@@ -115,7 +115,7 @@ $UserFromAD | Move-ADObject -TargetPath $DestinationOU
 #endregion ActiveDirectory
 
 #region Azure
-Write-Output "Performing Azure Steps"
+Write-Host "Performing Azure Steps" -ForegroundColor Cyan -BackgroundColor Black
 
 #Revoke all sessions
 Revoke-MgUserSign -UserId $MgUser.UserPrincipalName
@@ -127,17 +127,17 @@ Get-MobileDevice -Mailbox $UserFromAD.UserPrincipalName | ForEach-Object { Remov
 $365Mailbox | Set-Mailbox -Type Shared
 
 # Grant User FullAccess to Mailbox
-$UserAccessConfirmation = Read-Host -Prompt "Would you like to add FullAccess permissions to mailbox to $($UserFromAD.UserPrincipalName)? (Y/N)"
+$UserAccessConfirmation = $(Write-Host "Would you like to add FullAccess permissions to mailbox to $($UserFromAD.UserPrincipalName)? (Y/N)" -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
 
 if ($UserAccessConfirmation -eq 'y') {
 
-    $UserAccess = Read-Host -Prompt "Enter the email address of FullAccess recipient"
+    $UserAccess = $(Write-Output "Enter the email address of FullAccess recipient" -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
     try { 
         $GetAccessUser = get-mailbox $UserAccess -ErrorAction Stop
         $GetAccessUserCheck = 'yes'
     }
     catch { 
-	Write-Output "User mailbox $UserAccess not found. Skipping access rights setup"
+	Write-Host "User mailbox $UserAccess not found. Skipping access rights setup" -ForegroundColor Red -BackgroundColor Black
 	$GetAccessUserCheck = 'no'
 	}   
 } Else {
@@ -145,39 +145,39 @@ if ($UserAccessConfirmation -eq 'y') {
 }
 
 if ($GetAccessUserCheck -eq 'yes') { 
-    Write-Output "Adding Full Access permissions for $($GetAccessUser.PrimarySmtpAddress) to $($UserFromAD.UserPrincipalName)"
+    Write-Host "Adding Full Access permissions for $($GetAccessUser.PrimarySmtpAddress) to $($UserFromAD.UserPrincipalName)" -ForegroundColor Cyan -BackgroundColor Black
     Add-MailboxPermission -Identity $UserFromAD.UserPrincipalName -User $UserAccess -AccessRights FullAccess -InheritanceType All -AutoMapping $false }
 
 # Set Mailbox forwarding address 
-$UserFwdConfirmation = Read-Host -Prompt "Would you like to forward users email? (Y/N)"
+$UserFwdConfirmation = $(Write-Host "Would you like to forward users email? (Y/N)" -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
 
 if ($UserFwdConfirmation -eq 'y') {
 
-    $UserFWD = Read-Host -Prompt "Enter the email address of forward recipient"
+    $UserFWD = $(Write-Host "Enter the email address of forward recipient"  -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
     try { 
         $GetFWDUser = get-mailbox $UserFWD -ErrorAction Stop 
         $GetFWDUserCheck = 'yes'
-        Write-Output "Applying forward from $($UserFromAD.UserPrincipalName) to $($GetFWDUser.PrimarySmtpAddress)"
+        Write-Host "Applying forward from $($UserFromAD.UserPrincipalName) to $($GetFWDUser.PrimarySmtpAddress)" -ForegroundColor Cyan -BackgroundColor Black
     }
     catch { 
-	Write-Output "User mailbox $UserFWD not found. Skipping mailbox forward"
+	Write-Host "User mailbox $UserFWD not found. Skipping mailbox forward" -ForegroundColor Red -BackgroundColor Black
 	$GetFWDUserCheck = 'no'
 	}
     
 } Else {
-    Write-Output "Skipping mailbox forwarding"
+    Write-Host "Skipping mailbox forwarding" -ForegroundColor Cyan -BackgroundColor Black
 }
 
 if ($GetFWDUserCheck -eq 'yes') { Set-Mailbox $UserFromAD.UserPrincipalName -ForwardingAddress $UserFWD -DeliverToMailboxAndForward $False }
 
 #Find Azure only groups
 
-$AllAzureGroups = (Get-MgUserMemberOf -UserId $MgUser.UserPrincipalName).Id  | Where-Object {$_.AdditionalProperties['@odata.type'] -ne '#microsoft.graph.directoryRole'} | `
+$AllAzureGroups = Get-MgUserMemberOf -UserId $MgUser.UserPrincipalName  | Where-Object {$_.AdditionalProperties['@odata.type'] -ne '#microsoft.graph.directoryRole'} | `
         ForEach-Object { @{ GroupId=$_.Id}} | Get-MgGroup | Where-Object {$_.OnPremisesSyncEnabled -eq $NULL} | Select-Object DisplayName, SecurityEnabled, Mail, Id
 
 $Localpath = 'C:\Temp'
 
-$UserGroupsBackupConfirmation = Read-Host -Prompt "Would you like to backup user groups? (Y/N)"
+$UserGroupsBackupConfirmation = $(Write-Host "Would you like to backup user groups? (Y/N)" -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
 
 if ($UserGroupsBackupConfirmation -eq 'y') {
 
@@ -189,15 +189,15 @@ if ($UserGroupsBackupConfirmation -eq 'y') {
         New-Item -Path $Localpath -ItemType Directory
     }
     
-    Write-Output "Checking to see if User Group export exists"
+    Write-Host "Checking to see if User Group export exists" -ForegroundColor Cyan -BackgroundColor Black
     
     if ( Get-ChildItem -Path c:\temp | Where-Object {$_.Name -like 'User_Groups_Id.csv'} ) { 
-        Write-Output "Previous export exists. Please backup and then confirm removal."
+        Write-Host "Previous export exists. Please backup and then confirm removal." -ForegroundColor Red -BackgroundColor Black
         Remove-Item -Path C:\temp\User_Groups_Id.csv -Confirm}
     
     $AllAzureGroups | Export-Csv c:\temp\User_Groups_Id.csv -NoTypeInformation
     
-    Write-Output "Export User Groups Completed. Path: C:\temp\User_Groups_Id.csv"
+    Write-Host "Export User Groups Completed. Path: C:\temp\User_Groups_Id.csv" -ForegroundColor Cyan -BackgroundColor Black
 
 }
 
@@ -214,7 +214,7 @@ Foreach ($365Group in $AllAzureGroups) {
 #Get user licenses 
 $AllLicenses = Get-MgUserLicenseDetail -UserId $MgUser.Id
 
-$UserLicensesBackupConfirmation = Read-Host -Prompt "Would you like to backup user licenses? (Y/N)"
+$UserLicensesBackupConfirmation = $(Write-Host "Would you like to backup user licenses? (Y/N)" -ForegroundColor Yellow -BackgroundColor black -NoNewline; Read-Host)
 
 if ($UserLicensesBackupConfirmation -eq 'y') {
 
@@ -226,20 +226,20 @@ if ($UserLicensesBackupConfirmation -eq 'y') {
         New-Item -Path $Localpath -ItemType Directory
     }
     
-    Write-Output "Checking to see if User license export exists"
+    Write-Host "Checking to see if User license export exists" -ForegroundColor Cyan -BackgroundColor Black
     
     if ( Get-ChildItem -Path c:\temp | Where-Object {$_.Name -like 'User_License_Id.csv'} ) { 
-        Write-Output "Previous export exists. Please backup and then confirm removal."
+        Write-Host "Previous export exists. Please backup and then confirm removal." -ForegroundColor Red -BackgroundColor Black
         Remove-Item -Path C:\temp\User_License_Id.csv -Confirm}
     
     $AllLicenses | Export-Csv c:\temp\User_License_Id.csv -NoTypeInformation
     
-    Write-Output "Export User Licenses Completed. Path: C:\temp\User_License_Id.csv"
+    Write-Host "Export User Licenses Completed. Path: C:\temp\User_License_Id.csv" -ForegroundColor Cyan -BackgroundColor Black
 
 }
 
 #Remove Licenses
-Write-Output "Starting removal of user licenses."
+Write-Host "Starting removal of user licenses." -ForegroundColor Cyan -BackgroundColor Black
 
 Get-MgUserLicenseDetail -UserId $MgUser.Id | Where-Object `
    {($_.SkuPartNumber -ne "O365_BUSINESS_ESSENTIALS" -and $_.SkuPartNumber -ne "SPE_E3" -and $_.SkuPartNumber -ne "SPB" -and $_.SkuPartNumber -ne "EXCHANGESTANDARD") } `
@@ -247,11 +247,11 @@ Get-MgUserLicenseDetail -UserId $MgUser.Id | Where-Object `
 
 Get-MgUserLicenseDetail -UserId $MgUser.Id | ForEach-Object { Set-MgUserLicense -UserId $MgUser.Id -AddLicenses @() -RemoveLicenses $_.SkuId }
 
-Write-Output "Removal of user licenses completed."
+Write-Host "Removal of user licenses completed." -ForegroundColor Cyan -BackgroundColor Black
 
 #endregion Office365
 
 #Start AD Sync cycle
 Start-ADSyncSyncCycle -PolicyType Delta
 
-Write-Output "User $($user) should now be disabled unless any errors occurred during the process."
+Write-Host "User $($user) should now be disabled unless any errors occurred during the process." -ForegroundColor Cyan -BackgroundColor Black
