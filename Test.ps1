ab833b6acd7a8dd73e0df2cbbc47c236013b1af5
Chris Williams <ryanchrisw@gmail.com>
2022-08-17 00:11:08 -0400

    Create Test.ps1

---
+++ Test.ps1
@@ -0,0 +1,11 @@
+$LatestVersionUrl = (Invoke-WebRequest https://haveibeenpwned.com/Passwords -MaximumRedirection 0).Links | Where-Object {$_.href -like "*pwned-passwords-ntlm-ordered-by-hash-v*.7z"} | Select-Object -expand href
+
+#Variables built out for script 
+$LatestVersionZip = $($LatestVersionUrl -replace '[a-zA-Z]+://[a-zA-Z]+\.[a-zA-Z]+\.[a-zA-Z]+/[a-zA-Z]+/')
+$LatestVersionTXT = $($LatestVersionZip -replace '.7z') + '.txt'
+$LatestVersionLog = $($LatestVersionZip -replace 'pwned-passwords-ntlm-ordered-by-hash-') 
+$LatestVersionLog = $($LatestVersionLog -replace '.7z')
+
+$PassProtectionPath = 'C:\Program Files\Lithnet\Active Directory Password Protection\'
+
+Write-Output "URL is $LatestVersionUrl . Log version is $LatestVersionLog. TXT is $($LatestVersionTXT)."
