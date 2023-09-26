bdaea08503cc796b7f959e658a1f279be5a8879e
Chris Williams <ryanchrisw@gmail.com>
2023-09-25 20:40:28 -0400

    commit

---
+++ ActiveDirectory/Update-AdUserAttributes.ps1
@@ -0,0 +1,34 @@
+#CSV Headers - EmailAddress,Title,Department,MobilePhone,OfficePhone,ManagerEmailAddress
+
+Import-CSV -Path "C:\scripts\user_data.csv" | Foreach-Object {
+
+    $ADUser = Get-ADUser -Filter "EmailAddress -eq '$($_.EmailAddress)'" -Properties * 
+    
+    if ($_.Title) {     
+        $ADUser | Set-ADUser -Title $_.Title 
+        Write-Host "Updated Title for $($ADUser.SamAccountName)"
+    }
+
+    if ($_.Department) {     
+        $ADUser | Set-ADUser -Department $_.Department
+        Write-Host "Updated Department for $($ADUser.SamAccountName)"
+    }
+    
+    if ($_.MobilePhone) {     
+        $ADUser | Set-ADUser -Mobile $_.MobilePhone
+        Write-Host "Updated Mobile for $($ADUser.SamAccountName)"
+    }
+
+    if ($_.OfficePhone) {     
+        $ADUser | Set-ADUser –OfficePhone $_.OfficePhone
+        Write-Host "Updated OfficePhone for $($ADUser.SamAccountName)"
+    }
+
+    if ($_.ManagerEmailAddress) {
+        $managerDN = (Get-ADUser -Filter "EmailAddress -eq '$($_.ManagerEmailAddress)'").DistinguishedName
+        $ADUser | Set-ADUser -Manager $managerDN
+        Write-Host "Updated Manager for $($ADUser.SamAccountName)"
+    }
+
+}
+
---
+++ ActiveDirectory/Update-MgUserAttributes.ps1
@@ -0,0 +1,40 @@
+#CSV Headers - EmailAddress,Title,Department,MobilePhone,OfficePhone,ManagerEmailAddress
+
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes "User.Read.All", "Directory.ReadWrite.All"
+
+Import-CSV -Path "C:\scripts\user_data.csv" | Foreach-Object {
+
+    $UserId = (Get-MgUser -Filter "EmailAddress eq '$($_.EmailAddress)'").Id
+
+     'Technical Alignment Manager'
+    
+    if ($_.Title) {     
+        Update-MgUser -UserId $UserId -JobTitle $_.Title 
+        Write-Host "Updated Title for $($ADUser.SamAccountName)"
+    }
+
+    if ($_.Department) {     
+        Update-MgUser -UserId $UserId Department $_.Department
+        Write-Host "Updated Department for $($ADUser.SamAccountName)"
+    }
+    
+    if ($_.MobilePhone) {     
+        Update-MgUser -UserId $UserId -Mobile $_.MobilePhone
+        Write-Host "Updated Mobile for $($ADUser.SamAccountName)"
+    }
+
+    if ($_.OfficePhone) {     
+        Update-MgUser -UserId $UserId –OfficePhone $_.OfficePhone
+        Write-Host "Updated OfficePhone for $($ADUser.SamAccountName)"
+    }
+
+    if ($_.ManagerEmailAddress) {
+        $ManagerBinding = @{
+            "@odata.bind" = @("https://graph.microsoft.com/v1.0/users/"+$(Get-MgUser -Filter "UserPrincipalName eq '$($Manager)'").Id)
+        }
+        Set-MgUserManagerByRef -UserId $UserId -AdditionalProperties $ManagerBinding
+        Write-Host "Updated Manager for $($ADUser.SamAccountName)"
+    }
+
+}
