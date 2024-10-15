f3653c6fbf0af5b78c5f6dcbae5274ff293e1f70
tresnoface <ryanchrisw@gmail.com>
2024-10-15 09:04:23 -0400

    commit

--- ActiveDirectory/ADPasswordProtection/Install-ADPasswordProtection.ps1
+++ ActiveDirectory/ADPasswordProtection/Install-ADPasswordProtection.ps1
@@ -222,7 +222,7 @@ Function Install-ADPasswordProtection {
         #region Download and Install
         $URI = "https://github.com/lithnet/ad-password-protection/releases/latest"
 
-        $latestRelease = Invoke-WebRequest $URI -Headers @{"Accept" = "application/json" } -UseBasicParsing
+        $latestRelease = Invoke-WebRequest $URI -Headers @{"Accept" = "application/json" }
         $json = $latestRelease.Content | ConvertFrom-Json
         $latestVersion = $json.tag_name
 
@@ -236,7 +236,7 @@ Function Install-ADPasswordProtection {
         #Start-Process -FilePath C:\temp\$BuildExe -Wait
         $arguments ="/extract /exenoui"
         $install = "C:\temp\LithnetPasswordProtection.msi"
-        
+
         Start-Process -FilePath C:\temp\$BuildExe $arguments -Wait
         Start-Process -FilePath $env:systemroot\system32\msiexec.exe "/i $install /qn /quiet /norestart" -Wait;
 
